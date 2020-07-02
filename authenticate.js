2a324bc628b3ec0815782c96e2f0369979fc24be
Rittick chakraborty <rc.laden123@gmail.com>
2020-07-02 16:34:14 +0530

    simple passport jwt 2

--- authenticate.js
+++ authenticate.js
@@ -6,7 +6,7 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
-var config = require('./config.js');
+var config = require('./config');
 
 
 
