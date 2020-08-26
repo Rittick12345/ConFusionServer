e061cb93710f2d4320bd34a792b4e7b493c781d4
Rittick Chakraborty <rittick19971997@gmail.com>
2020-08-27 01:33:48 +0530

    passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
-}
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId: '941112706371347',
+        clientSecret: '1d01ed523f535bceac76f1947c1d138d'
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -65,6 +65,14 @@ router.post('/login',cors.corsWithOption, passport.authenticate('local'),(req, r
   res.json({success: true,token: token, status: 'You are successfully logged in as verified user!!!'});
   }
 });
+router.get('/facebook/token',passport.authenticate('facebook-token'), (req, res) =>{
+  if(req.user){
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true,token: token, status: 'You are successfully logged in as facebook verified user!!!'});
+  }
+});
 
 router.get('/logout',(req, res,next) =>{
   
