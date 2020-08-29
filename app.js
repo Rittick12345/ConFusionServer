fc16b17685f9372ea12254a3e38308f229e73982
Rittick Chakraborty <rittick19971997@gmail.com>
2020-08-29 21:30:36 +0530

    adding promotion router

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ var passport = require('passport');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require ('./routes/dishRouter');
-//var promoRouter = require('./routes/promoRouter');
+var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
 var favouriteRouter = require('./routes/favouriteRouter');
 
 var dishes = require('./models/dishes');
 var User = require ('./models/user');
-//var promotions = require('./models/promotions');
+var promotions = require('./models/promotions');
 var leaders = require('./models/leaders');
 var authenticate = require('./authenticate');
 var config = require('./config');
@@ -75,7 +75,7 @@ app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/imageUpload',uploadRouter);
 app.use('/favourites',favouriteRouter);
-//app.use('/promotions', promoRouter);
+app.use('/promotions', promoRouter);
 app.use('/leaders',leaderRouter);
 
 
